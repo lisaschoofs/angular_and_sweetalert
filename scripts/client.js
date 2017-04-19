6183905d680a3d7d9e509de00a1f8d7318ea192b
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-18 21:05:37 -0500

    sweet alert sourced, alert on page load

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,10 @@
+
+
 var myApp = angular.module('myApp', []);
 
 myApp.controller('OneController', [function(){
   console.log('OneController loaded!');
+  swal('Good job!', 'Sweet Alert is set up!', 'success');
 }]);
 
 // swal("Hello world!");
